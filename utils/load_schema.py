98f3f56c0243f9deef2cb9bd760a10ba0caa20d9
Yana Zaitseva <zaitsevayg@gmail.com>
2025-12-10 17:25:12 +0300

    Readme + schema validation

--- utils/load_schema.py
+++ utils/load_schema.py
@@ -3,8 +3,10 @@ import os
 
 
 def load_schema(filename: str):
-    schema_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname((os.path.abspath(__file__))))),
-                               'schema', filename)
-    with open(schema_path) as file:
+    project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    schema_path = os.path.join(project_root, 'schema', filename)
+
+
+    with open(schema_path, encoding='utf-8') as file:  # Указываем кодировку
         schema = json.load(file)
         return schema
